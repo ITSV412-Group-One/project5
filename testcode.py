334e6f1038a9d5a77b49c6028e6d112c913b2632
m0chic333 <molivia333@tamu.edu>
2023-10-16 17:54:11 -0500

    testcode.py

--- testcode.py
+++ testcode.py
@@ -5,11 +5,36 @@ BASE_URL = "http://localhost:4000"
 
 def test_fibonacci():
     print("Testing /fibonacci endpoint...")
+
     r = requests.get(f"{BASE_URL}/fibonacci/10")
-    data = r.json()
-    
+    assert r.status_code == 200
+    assert r.json()["input"] == 10 
+    assert r.json()["output"] == [1, 1, 2, 3, 5, 8]
+
+    r = requests.get(f"{BASE_URL}/fibonacci/-1")
     assert r.status_code == 200 
-    assert data["input"] == 10
-    assert data["output"] == [1, 1, 2, 3, 5, 8]
-    
+    assert "Input must be a positive integer" in r.text
+
     print("Fibonacci test passed!")
+
+def test_prime():
+    print("Testing /is-prime endpoint...")
+
+    r = requests.get(f"{BASE_URL}/is-prime/7")
+    assert r.status_code == 200 
+    assert r.json()["input"] == 7
+    assert r.json()["output"] == True
+
+    r = requests.get(f"{BASE_URL}/is-prime/4")
+    assert r.json()["output"] == False
+
+    print("Prime test passed!")
+    
+if __name__ == "__main__":
+    test_fibonacci()
+    test_prime()
+    
+    if True: #assuming all tests passed
+        sys.exit(0) 
+    else:
+        sys.exit(1)
