ca3130fd26d7b06868d1f9e0b3183b2ac75519e7
m0chic333 <molivia333@tamu.edu>
2023-10-16 17:13:24 -0500

    restAPI.py

--- restAPI.py
+++ restAPI.py
@@ -16,6 +16,15 @@ app = Flask(__name__)
 def hello_world():
   return "<p> This is the Home page </p>"
 
+# Mel string sunction 
+@app.route("/hash/<string:input_string>")
+def hash_string(input_string):
+
+  md5 = hashlib.md5(input_string.encode())
+  output = md5.hexdigest()
+
+  return jsonify({"input": input_string, "output": output})
+
 # port 4000
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=4000)
