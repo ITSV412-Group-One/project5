77574f9f6011a92acd08bab91a7b918e4ed057ce
m0chic333 <molivia333@tamu.edu>
2023-10-16 13:12:25 -0500

    testcode.py

--- testcode.py
+++ testcode.py
@@ -2,3 +2,14 @@ import requests
 import sys
 
 BASE_URL = "http://localhost:4000" 
+
+def test_fibonacci():
+    print("Testing /fibonacci endpoint...")
+    r = requests.get(f"{BASE_URL}/fibonacci/10")
+    data = r.json()
+    
+    assert r.status_code == 200 
+    assert data["input"] == 10
+    assert data["output"] == [1, 1, 2, 3, 5, 8]
+    
+    print("Fibonacci test passed!")
