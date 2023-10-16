b117be7583a71c84c904ffd5fc93967161a03312
m0chic333 <molivia333@tamu.edu>
2023-10-16 17:24:19 -0500

    restAPI.py

--- restAPI.py
+++ restAPI.py
@@ -62,6 +62,27 @@ def get_fibonacci(input_number):
     "output": sequence
   })
   
+# Maya 
+def is_prime(num):
+  if num <= 1:
+    return False
+
+  for i in range(2, int(sqrt(num)) + 1):
+    if num % i == 0:
+      return False
+  
+  return True
+
+@app.route("/is-prime/<int:input_number>")
+def check_prime(input_number):
+
+  result = is_prime(input_number)
+
+  return jsonify({
+    "input": input_number, 
+    "output": result
+  })
+  
 
 # port 4000
 if __name__ == "__main__":
