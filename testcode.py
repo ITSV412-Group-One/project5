049e9bc49cd42c3716bf304455eb2c45aedf0bfe
m0chic333 <molivia333@tamu.edu>
2023-10-16 18:06:35 -0500

    testcode.py

--- testcode.py
+++ testcode.py
@@ -4,37 +4,31 @@ import sys
 BASE_URL = "http://localhost:4000" 
 
 def test_fibonacci():
-    print("Testing /fibonacci endpoint...")
+  print("Testing /fibonacci endpoint...")
 
-    r = requests.get(f"{BASE_URL}/fibonacci/10")
-    assert r.status_code == 200
-    assert r.json()["input"] == 10 
-    assert r.json()["output"] == [1, 1, 2, 3, 5, 8]
+  r = requests.get(f"{BASE_URL}/fibonacci/10")
+  
+  print(f"Status code: {r.status_code}")
+  print(f"Response: {r.text}")
+  
+  assert r.status_code == 200
+  assert r.json()["input"] == 10
+  assert r.json()["output"] == [1, 1, 2, 3, 5, 8]
 
-    r = requests.get(f"{BASE_URL}/fibonacci/-1")
-    assert r.status_code == 200 
-    assert "Input must be a positive integer" in r.text
+  r = requests.get(f"{BASE_URL}/fibonacci/-1")
+  
+  print(f"Status code: {r.status_code}")
+  print(f"Response: {r.text}")
+  
+  assert r.status_code == 200
+  assert "Input must be a positive integer" in r.text
 
-    print("Fibonacci test passed!")
+  print("Fibonacci test passed!")
 
-def test_prime():
-    print("Testing /is-prime endpoint...")
-
-    r = requests.get(f"{BASE_URL}/is-prime/7")
-    assert r.status_code == 200 
-    assert r.json()["input"] == 7
-    assert r.json()["output"] == True
-
-    r = requests.get(f"{BASE_URL}/is-prime/4")
-    assert r.json()["output"] == False
-
-    print("Prime test passed!")
-    
 if __name__ == "__main__":
-    test_fibonacci()
-    test_prime()
-    
-    if True: #assuming all tests passed
-        sys.exit(0) 
-    else:
-        sys.exit(1)
+  test_fibonacci()
+
+  if True: 
+    sys.exit(0)
+  else:
+    sys.exit(1)
