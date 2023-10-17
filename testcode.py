1ea68734d31a8d816b533c3453b7d40b8c6ab919
m0chic333 <molivia333@tamu.edu>
2023-10-16 21:04:18 -0500

    testcode.py

--- testcode.py
+++ testcode.py
@@ -7,11 +7,17 @@ def test_fibonacci_positive():
   response = requests.get(f"{URL}/fibonacci/6")
   assert response.status_code == 200
   assert response.json() == [1, 1, 2, 3, 5]
+  
+  print(f"Status code: {r.status_code}")
+  print(f"Response: {r.text}")
 
 def test_fibonacci_negative():
   print("Testing /fibonacci endpoint...")
   response = requests.get(f"{URL}/fibonacci/-1") 
   assert response.status_code == 400
+  
+  print(f"Status code: {r.status_code}")
+  print(f"Response: {r.text}")
   
   if __name__ == "__main__":
     test_fibonacci_positive()
