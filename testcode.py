52a8a6a1c9041438a46e5887d8b0a04ae9b4129f
m0chic333 <molivia333@tamu.edu>
2023-10-16 19:36:01 -0500

    testcode.py

--- testcode.py
+++ testcode.py
@@ -1,5 +1,5 @@
 import requests
-import sys 
+from requests import codes
 
 BASE_URL = "http://localhost:4000"
 
@@ -7,30 +7,27 @@ def test_fibonacci():
 
   print("Testing /fibonacci endpoint...")
 
+  # Positive case
   r = requests.get(f"{BASE_URL}/fibonacci/10")
-    
-  print(f"Status code: {r.status_code}")
-  print(f"Response: {r.text}")
-
-  if r.status_code == 200:
-    assert r.json()["input"] == 10 
-    assert r.json()["output"] == [1, 1, 2, 3, 5, 8]
-
-  else:
-    print("Got unexpected status code, skipping assertions")
-
-  r = requests.get(f"{BASE_URL}/fibonacci/-1")
-   
-  print(f"Status code: {r.status_code}")
-  print(f"Response: {r.text}")
-
-  if r.status_code == 400:  
-    assert "Input must be positive" in r.text
   
-  else:
-    print("Got unexpected status code, skipping assertions")
+  assert r.status_code == codes.ok 
+  assert r.json()["input"] == 10
+  assert r.json()["output"] == [1, 1, 2, 3, 5, 8]
+
+  # Negative case  
+  r = requests.get(f"{BASE_URL}/fibonacci/-1")
+
+  assert r.status_code == codes.bad_request
+  assert "Input must be positive" in r.json()["error"]
+
+  # Edge case
+  r = requests.get(f"{BASE_URL}/fibonacci/0")  
+
+  assert r.status_code == codes.ok
+  assert r.json()["output"] == []
 
   print("Fibonacci test passed!")
+
 
 if __name__ == "__main__":
   
