504bf1a80bf48ae5bedf2558135880028ced60be
m0chic333 <molivia333@tamu.edu>
2023-10-16 21:18:25 -0500

    testcode.py

--- testcode.py
+++ testcode.py
@@ -1,35 +1,37 @@
 import requests
+import sys
 
 URL = "http://localhost:4000"
 
 def test_fibonacci_positive():
 
-  print("Testing /fibonacci endpoint positive case...")
-  
+  print("Testing /fibonacci positive case...")
+
   response = requests.get(f"{URL}/fibonacci/6")
 
   print(response.json())
 
   assert response.status_code == 200
-  assert response.json() == [1, 1, 2, 3, 5]
+  assert response.json() == [1, 1, 2, 3, 5, 8]
 
   print("Positive case passed!")
 
 def test_fibonacci_negative():
 
-  print("Testing /fibonacci endpoint negative case...")
+  print("Testing /fibonacci negative case...")  
 
   response = requests.get(f"{URL}/fibonacci/-1")
 
   assert response.status_code == 400
 
   print("Negative case passed!")
-  
-if __name__ == "__main__":
-    test_fibonacci_positive()
-    test_fibonacci_negative()
 
-if True:
+if __name__ == "__main__":
+  
+  test_fibonacci_positive()
+  test_fibonacci_negative()
+
+  if True:
     sys.exit(0)
-else:
+  else: 
     sys.exit(1)
