0ae6ff9e04a4382229c30b3c8aaa44c1582fd74f
m0chic333 <molivia333@tamu.edu>
2023-10-16 21:00:51 -0500

    testcode.py

--- testcode.py
+++ testcode.py
@@ -1,43 +1,12 @@
 import requests
-import pytest
-from requests import codes
 
-BASE_URL = "http://localhost:4000"
+URL = "http://localhost:4000"
 
-def test_fibonacci():
+def test_fibonacci_positive():
+  response = requests.get(f"{URL}/fibonacci/6")
+  assert response.status_code == 200
+  assert response.json() == [1, 1, 2, 3, 5]
 
-  # Positive test
-  response = requests.get(f"{BASE_URL}/fibonacci/10")
-  assert response.status_code == codes.ok
-  assert response.json()["input"] == 10
-  assert response.json()["output"] == [1, 1, 2, 3, 5, 8]
-
-  # Negative test
-  response = requests.get(f"{BASE_URL}/fibonacci/-1")
-  assert response.status_code == codes.bad_request
-  assert "Input must be positive" in response.json()["error"]
-
-  # Edge case
-  response = requests.get(f"{BASE_URL}/fibonacci/0")
-  assert response.json()["output"] == []
-
-  # Invalid input type
-  response = requests.get(f"{BASE_URL}/fibonacci/foo")
-  assert response.status_code == codes.bad_request
-
-  print("Fibonacci tests passed!")
-
-# Additional test functions for each endpoint
-
-def run_tests():
-  functions = [test_fibonacci] # etc
-
-  for test in functions:
-    test()
-
-  # Return 0 if all passed
-  return 0
-
-if __name__ == "__main__":
-  exit_code = run_tests()
-  sys.exit(exit_code)
+def test_fibonacci_negative():
+  response = requests.get(f"{URL}/fibonacci/-1") 
+  assert response.status_code == 400
