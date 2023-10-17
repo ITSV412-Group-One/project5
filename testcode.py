38c2a2104b71b368f43873266c0c0507b4ea1a07
m0chic333 <molivia333@tamu.edu>
2023-10-17 12:00:44 -0500

    testcode.py

--- testcode.py
+++ testcode.py
@@ -12,30 +12,30 @@ def api_url():
 ])
 
 # a function to test the /md5/<string> endpoint
-def test_md5_endpoint():
-   test_cases = [
-      ("Hello World", "b10a8db164e07541005b7a99be72e3fe5"),
-      ("%20", "7215ee9c7d9dc229d2921a40e899ec5f"),
-      ("test String", "bd08ba3c982eaad768602536fb8e1184")
-   ]
+@pytest.mark.parametrize(
+    "test_string, expected_md5_hash",
+    [
+        ("Hello World", "b10a8db164e07541005b7a99be72e3fe5"),
+        ("%20", "7215ee9c7d9dc229d2921a40e899ec5f"),
+        ("test String", "bd08ba3c982eaad768602536fb8e1184")
+    ]
+)
+def test_md5_endpoint(test_string, expected_md5_hash):
+    url = f"{base_url}/md5/{test_string}"
+    response = requests.get(url)
+    assert response.status_code == 200, f"MD5 endpoint returned a non-200 status code for input: {test_string}"
 
-   for test_string, expected_md5_hash in test_cases:
-      url = f"{base_url}/md5/{test_string}"
-      response = requests.get(url)
-      if response.status_code != 200:
-         print(f"MD5 endpoint returned a non-200 status code for input: {test_string}")
-         continue  # skip to the next test case
+    actual_response = json.loads(response.text)
+    actual_md5_hash = actual_response['output']
 
-      actaul_response = json.loads(response.text)
-      actual_md5_hash = actaul_response['output']
+    assert actual_md5_hash == expected_md5_hash, f"MD5 test failed for input: {test_string}"
 
-      if actual_md5_hash == expected_md5_hash:
-         print(f"MD5 test passed for input: {test_string}")
-      else:
-         print(f"MD5 test failed for input: {test_string}")
-         log_comparison(expected_md5_hash, actual_md5_hash, "MD5 hash does not match")
-
-
+# Utility function to log the comparison (optional)
+def log_comparison(expected, actual, message):
+    print(message)
+    print(f"Expected: {expected}")
+    print(f"Actual: {actual}")
+    
 # Maya 
 
 # test fibonacci code - Maya
