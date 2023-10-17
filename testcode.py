de2cdb9974caaa0efeb853818f979491053156b3
m0chic333 <molivia333@tamu.edu>
2023-10-16 21:38:38 -0500

    testcode.py

--- testcode.py
+++ testcode.py
@@ -1,40 +1,23 @@
 import requests
-import sys
+import pytest
 
-URL = "http://localhost:4000"
+@pytest.fixture
+def api_url():
+  return "http://localhost:5000"
 
-def test_fibonacci_positive():
+@pytest.mark.parametrize("n,expected", [
+  (6, [1, 1, 2, 3, 5, 8]),
+  (-1, {"error": "Invalid input"})  
+])
+def test_fibonacci(api_url, n, expected):
 
-  print("Testing /fibonacci positive case...")
-
-  response = requests.get(f"{URL}/fibonacci/6")
-
-  print(response.json())
+  url = f"{api_url}/fibonacci/{n}"
+  response = requests.get(url)
 
   assert response.status_code == 200
-  assert response.json() == [1, 1, 2, 3, 5, 8]
+  assert response.json() == expected
 
-  print("Positive case passed!")
+def test_invalid_input(api_url):
 
-def test_fibonacci_negative():
-
-  print("Testing /fibonacci negative case...")  
-
-  response = requests.get(f"{URL}/fibonacci/-1")
-
-  print(response.status_code)
-
-  assert response.status_code >= 400
-  assert "Invalid input" in response.text
-  
-  print("Negative case passed!")
-
-if __name__ == "__main__":
-  
-  test_fibonacci_positive()
-  test_fibonacci_negative()
-
-  if True:
-    sys.exit(0)
-  else: 
-    sys.exit(1)
+  url = f"{api_url}/fibonacci/foo"
+  response = requests.get(url)
