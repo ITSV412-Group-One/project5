9f14e3ba28adc6d4654ba93cb0af287855705fab
m0chic333 <molivia333@tamu.edu>
2023-11-12 23:24:18 -0600

    restAPI_cli.py

--- restAPI_cli.py
+++ restAPI_cli.py
@@ -4,53 +4,48 @@ import requests
 import json
 import sys
 
-if os.environ.get('DOCKER_NETWORK_MODE') == 'backend':
-  BASE_URL = 'http://restapi:4000'
-else:
-  BASE_URL = 'http://127.0.0.1:8000' 
-
-parser = argparse.ArgumentParser(description='CLI for API')
+parser = argparse.ArgumentParser()
 parser.add_argument('command')
-parser.add_argument('args', nargs='?')
+parser.add_argument('arg', nargs='?')
+args = parser.parse_args()
 
-def call_api(endpoint, args):
+command = args.command
+arg = args.arg
 
-  print(f"Command: {command}, Args: {args}")
+if os.environ.get('DOCKER_NETWORK_MODE') == 'backend':
+  BASE_URL = 'http://restapi:4000'  
+else:
+  BASE_URL = 'http://127.0.0.1:8000'
 
-  url = BASE_URL + '/' + endpoint
-  print(f"Request URL: {url}")
+def call_api(endpoint):
   
+  url = BASE_URL + '/' + endpoint
+  
+  print(f'Calling API: {url}')
+
   resp = requests.get(url)
 
   if resp.status_code == 200:
-    data = resp.json()
-    print(json.dumps(data, indent=2))
+    print(resp.json())
   else:
     print(f'Error: {resp.status_code}')
 
-  # Rest of function
 
 if __name__ == '__main__':
-  
-  args = parser.parse_args()
-  
-  command = args.command
-  command_args = args.args
 
-  # Test hardcoded endpoint
-  print('Testing hardcoded endpoint...')
-  call_api('hash/hello', None)
-  call_api('factorial/5', None) 
-  call_api('hash/hello', None)
-  call_api('fibonacci/5', None)
-  call_api('is-prime/5', None)
+  # Test hardcoded endpoints
+  print('Testing endpoints...')
+
+  call_api('hash/hello')
+  call_api('factorial/5')
+  call_api('fibonacci/5')
+  call_api('is-prime/7')
 
   if arg:
     endpoint = f'{command}/{arg}'
   else:
-    endpoint = command  
-
+    endpoint = command
+  
   print(f'Calling endpoint: {endpoint}')
 
-  # Call API with endpoint
   call_api(endpoint)
