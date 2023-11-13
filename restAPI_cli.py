ccc245431ac1a0d3b227040b7e8efc7c5033b70a
m0chic333 <molivia333@tamu.edu>
2023-11-12 21:55:26 -0600

    restAPI_cli.py

--- restAPI_cli.py
+++ restAPI_cli.py
@@ -14,22 +14,27 @@ parser.add_argument('command')
 parser.add_argument('args', nargs='?')
 
 def call_api(endpoint, args):
+
+  print(f"Command: {command}, Args: {args}")
+
   url = BASE_URL + '/' + endpoint
+  print(f"Request URL: {url}")
+
   if args:
     resp = requests.post(url, json=args)
   else:
     resp = requests.get(url)
 
-  if resp.status_code != 200:
-    print(f"Error: {resp.status_code}")
-    return
-
-  data = resp.json()
-  print(json.dumps(data, indent=2))
+  # Rest of function
 
 if __name__ == '__main__':
+  
   args = parser.parse_args()
   command = args.command
   command_args = args.args
 
+  print("Testing hardcoded endpoint...")
+  call_api("users", None)
+
+  print("Calling command endpoint")
   call_api(command, command_args)
