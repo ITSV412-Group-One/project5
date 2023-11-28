d03ecac7cd8db2bc6ec7446f4650e5a6c1f317d0
m0chic333 <molivia333@tamu.edu>
2023-11-28 10:55:54 -0600

    testcode.py

--- testcode.py
+++ testcode.py
@@ -22,23 +22,3 @@ def pytest_runtest_logfinish(node_id):
     print(f"Finished test: {node_id}")
     
 
-# Fibonacci tests  
-@pytest.mark.parametrize("endpoint,status_code", [
-    ("/fibonacci/6", 200),
-    ("/fibonacci/invalid", 400)
-]) 
-def test_fibonacci_response_codes(api_url, endpoint, status_code):
-
-   url = f"{api_url}{endpoint}"
-   res = requests.get(url)
-@pytest.mark.parametrize("n,expected", [
-  (8, [0, 1, 1, 2, 3, 5, 8]),
-  (1, [0, 1]),
-])
-def test_fibonacci(api_url, n, expected):
-  url = f"{api_url}/fibonacci/{n}"
-  assert requests.get(url).json() == expected
-
-
-if __name__ == "__main__":
-  pytest.main(["-s", "testcode.py"])
