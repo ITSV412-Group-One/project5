cd4fca66fa209f668f78188d885f3a9f1ea0936c
m0chic333 <molivia333@tamu.edu>
2023-11-28 12:23:59 -0600

    testcode.py

--- restAPI_cli.py
+++ restAPI_cli.py
@@ -1,52 +1,137 @@
-# Command Line code
-import os
-import argparse
-import requests 
-import json
-import sys
+import requests
+from flask import Flask
+app = Flask(__name__)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('command')
-parser.add_argument('arg', nargs='?')
-args = parser.parse_args()
-
-command = args.command
-arg = args.arg
-
-if os.environ.get('DOCKER_NETWORK_MODE') == 'backend':
-  BASE_URL = 'http://restapi:4000'  
-else:
-  BASE_URL = 'http://127.0.0.1:8000'
-
-def call_api(endpoint):
-  
-  url = BASE_URL + '/' + endpoint
-  
-  print(f'Calling API: {url}')
-
-  resp = requests.get(url)
-
-  if resp.status_code == 200:
-    print(resp.json())
-  else:
-    print(f'Error: {resp.status_code}')
+host = REDIS_HOST 
+errors = 0
+import click
 
 
+@click.group()
+def cli():
+    pass
+	
+	
+@cli.command()
+@click.option('--phrase', default= 'hello',
+              help= 'The phrase to translate to md5')
+@click.argument('phrase')
+def md5(phrase):
+    """This will translate a given phrase to md5"""
+    t = requests.get(f'http://{host}/md5/{phrase}')
+    click.echo('This is the md5 encryption for %s:' % phrase)
+    click.echo(t.json()['output'])
+    pass
+#####
+
+
+@cli.command()
+@click.option('--number', default= 1,
+              help= 'The number to find factorials of')
+@click.argument('number')
+def factorial(number):
+    """This will calculate the factorial of a number"""
+    t = requests.get(f'http://{host}/factorial/{number}')
+    click.echo('This is the factorial for %d:' % number)
+    click.echo(t.json()['output'])
+    pass
+#######
+
+
+
+@cli.command()
+@click.option('--number', default= 1,
+              help= 'The number to find fibonacci sequence of')
+@click.argument('number')
+def fibonacci(number):
+    """This will return the fibonacci sequence of a number"""
+    t = requests.get(f'http://{host}/fibonacci/{number}')
+    click.echo('This is the fibonacci sequence for %d:' % number)
+    click.echo(t.json()['output'])
+    pass
+#######
+
+
+@cli.command()
+@click.option('--number', default= 1,
+              help= 'The number to be checked if prime')
+@click.argument('number')
+def is_prime(number):
+    """This will tell you if a given number is prime or not"""
+    t = requests.get(f'http://{host}/is-prime/{number}')
+    click.echo('Is %d a prime number:' % number)
+    print(t.json()['output'])
+    pass
+#####
+
+
+
+@cli.command()
+@click.option('--message', default= '1',
+              help= 'The message to send in slack')
+@click.argument('message')
+def slack_alert(message):
+    """This will send a slack alert and tell you if it sent successfully"""
+    t = requests.get(f'http://{host}/slack-alert/{message}')
+    print(t.json()['output'])
+    pass
+#####
+
+
+
+@cli.command()
+@click.option('--post', default= '',
+              help= 'post test')
+
+def post():
+    """Insert a single entry into the database"""
+    usr_key, usr_value = input("Enter a key followed by its value in the format of: key, value: ").split(", ")
+    
+    result = {'key': usr_key, 'value': usr_value}
+    t = requests.post(f'http://{host}/keyval', json=result)
+    print(t.json()['command'])
+    print(t.json()['result'])
+    print(t.json()['error'])
+    pass
+####
+
+
+
+@cli.command()
+@click.option('--string', default='',
+              help='get test')
+@click.argument('string')
+def get(string):
+    """Returns the entry associated with the key"""
+    t = requests.get(f'http://{host}/keyval/{string}')
+    click.echo('GET %s:' % string)
+    if t.json()['result'] is True:
+        print(t.json()['value'])
+    else:
+        print(t.json()['error'])
+    pass
+#####
+
+
+
+@cli.command()
+@click.option('--put', default= '',
+              help= 'put test')
+def put(put):
+    """Updates the entry associated with the key with the value provided"""
+    pass
+#####
+
+
+
+@cli.command()
+@click.option('--user_key', default= '',
+              help= 'delete test')
+def delete(user_key):
+    """Remove the entries associated with the keys provided."""
+    pass
+######
+
+ 
 if __name__ == '__main__':
-
-  # Test hardcoded endpoints
-  print('Testing endpoints...')
-
-  call_api('md5/hello')
-  call_api('factorial/5')
-  call_api('fibonacci/5')
-  call_api('is-prime/7')
-
-  if arg:
-    endpoint = f'{command}/{arg}'
-  else:
-    endpoint = command
-  
-  print(f'Calling endpoint: {endpoint}')
-
-  call_api(endpoint)
+   cli()
--- testcode.py
+++ testcode.py
@@ -1,23 +1,127 @@
 import requests
 import json
-import pytest
-import math
-from math import factorial
-from dotenv import load_dotenv
-import os
-load_dotenv()
+from flask import Flask, jsonify, escape, request, Response
+host = REDIS_HOST
+errors = 0
 
-@pytest.fixture  
-def api_url():
-  if os.environ.get('DOCKER_NETWORK_MODE') == 'backend':
-    return "'http://restapi:4000'"
-  else:
-    return "http://localhost:4000"
-  
-def pytest_configure(config):
-    config.rootdir = '/app/testcode.py'
+#all_tests = [
+#    '/md5/test',
+#    '/md5/hello%20world',
+#    '/factorial/4',
+#    '/factorial/20',
+#    '/fibonacci/443',
+#    '/is-prime/15'
+#    ]
 
-def test_api(api_url):
-    url = f"{api_url}/test"
-    res = requests.get(url)
+all_tests_dict = {  #IF U NEED SOMETHING TO DO: this dictonary needs to be populated with all endpoints and expected results
+    '/md5/test': '098f6bcd4621d373cade4e832627b4f6',
+    '/md5/hello%20world': '5eb63bbbe01eeed093cb22bb8f5acdc3',
+    '/md5': 404,
+    '/factorial/4': 24,
+    '/factorial/5': 120,
+    '/factorial/test': 404,
+    '/fibonacci/0':[0],
+    '/fibonacci/8':[0,1,1,2,3,5,8],
+    '/fibonacci/35':[0,1,1,2,3,5,8,13,21,34],
+    '/fibonacci/1':[0,1,1],
+    '/fibonacci/test': 404,
+    '/is-prime/1': False,
+    '/is-prime/2': True,
+    '/is-prime/5': True,
+    '/is-prime/6': False,
+    '/is-prime/15': False,
+    '/is-prime/37': True,
+    '/is-prime/one': 404,
+    '/slack-alert/test': True,
+    '/slack-alert/this%20is%20a': True
+    }
+keyval_tests_dict = {
+    '/keyval' : {'key':'test1', 'value':'1'}
+    
+    }
 
+
+#test = requests.get(f'http://{host}/md5/test')  #manual and worst way
+#expected_result_md5 = 'slkdfjsdlfesid34'
+#test.json() #ouitputs library from the request {input:tester,output:3523423f3243"}
+#if expected_result_md5 == test.json()['output']:
+#    print('OK')
+
+
+#for test in all_tests: #using a for loop to test a list of endpoints
+#    t = requests.get(f'http://{host}{test}')
+#    print(f'Status code: {t.status_code}')
+#    if t.status_code != 200:
+#        errors += 1
+        
+        
+for path, result in all_tests_dict.items(): #using a dict, best but most complicated way
+    print(f"Path: {path} / EXPECTED RESULT: {result}")
+    t = requests.get(f'http://{host}{path}')
+    if t.status_code == 200:
+        print(f"Actual Result: {t.json()['output']}")
+        if t.json()['output'] == result:
+            print("PASS\n")
+        else:
+            print("ERROR\n")
+            errors += 1
+            
+    elif t.status_code == 404:
+        
+        if result == 404:
+            print("PASS\n")
+        else:
+            print("ERROR\n")
+            errors += 1
+    else:
+            print("ERROR\n")
+            errors += 1
+    
+              
+for path, result in keyval_tests_dict.items(): #post
+    print(f"Path: {path} / Method: POST / json payload {result}")
+    t = requests.post(f'http://{host}{path}', json=result)
+    print(t.status_code)
+    if t.status_code == 200:
+        print("PASS\n")
+    else:
+        print("ERROR\n")
+        errors += 1
+ 
+
+              
+for path, result in keyval_tests_dict.items(): #GET
+    print(f"Path: {path} / Method: GET / json payload {result}")
+    t = requests.get(f'http://{host}{path}/test1') #hardcoded get string. Could string splice my Dict key to extract the key string
+    print(f"Status code: {t.status_code}")
+    if t.status_code == 200:
+        print("PASS\n")
+    else:
+        print("ERROR\n")
+        errors += 1       
+              
+for path, result in keyval_tests_dict.items(): #PUT
+    print(f"Path: {path} / Method: PUT / json payload 'key':'test1', 'value':'something'")
+    t = requests.put(f'http://{host}{path}', json={'key':'test1', 'value':'something'})
+    print(f"Status code: {t.status_code}")
+    if t.status_code == 200:
+        print("PASS\n")
+    else:
+        print("ERROR\n")
+        errors += 1       
+              
+for path, result in keyval_tests_dict.items(): #DELETE
+    print(f"Path: {path} / Method: DELETE / json payload 'key':'test1', 'value':'something'")
+    t = requests.delete(f'http://{host}{path}/test1')
+    print(f"Status code: {t.status_code}")
+    if t.status_code == 200:
+        print("PASS\n")
+    else:
+        print("ERROR\n")
+        errors += 1                     
+             
+
+              
+
+              
+print(f"Errors = {errors}")
