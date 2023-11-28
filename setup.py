3a52e32e526f5923a00e3e7422146c49f6b51a45
m0chic333 <molivia333@tamu.edu>
2023-11-28 13:00:31 -0600

    testcode.py

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ setup(
   name = 'test',
   version = '1.0',
   
-  #insert whatever py file this will use
   py_modules = ['test'],
   
   install_requires = ['Click', ],
   
-  #not sure what this is but hello stuff needs to be changed for our specifics
+  
   entry_points = '''
        [console_scripts]
        hello=hello:cli
